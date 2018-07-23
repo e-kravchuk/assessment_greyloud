b1c057050853ffbeaed9dcd0a53ef5a350911b61
Yegor <e.kravchuk@invento.by>
2018-07-23 19:01:27 +0300

    New version without while inside

--- GeoHelper.php
+++ GeoHelper.php
@@ -13,18 +13,47 @@ class GeoHelper
         if (count($countEl) > 100000) {
             return 'Error: max count of elements 100000';
         }
-
+        
         foreach ($A as $key => $value) {
             if (!is_integer($value)) return "Error: not integer element with key $key";
             if (0 > $value || $value > 100000000) return "Error: element with key $key out of range 1 - 100000000";
         }
+        
+        for ($m = $countEl - 1; $m >= 0; $m--) {  //Обрезаем конец массива, который не может содержать воды 
+            if ($A[$m] <= $A[$m - 1]) { 
+                unset($A[$m]);
+                $countEl--;
+            } 
+            else break; 
+        }
+        
+        for ($m = 0; $m < $countEl; $m++) {  //Обрезаем начало массива, который не может содержать воды 
+            if ($A[$m] <= $A[$m + 1]) { 
+                unset($A[$m]);
+                $countEl--;
+            }
+            else break;
+        }
 
         $result = 0;
+        $startPoint = $A[$i];
+        $endPoint = 0;
+        $heigth = 0;
+        $minimum = 0;
 
         for ($i = 0; $i < $countEl; $i++) {
-            $startPoint = 0;
-            $endPoint = 0;
+            
+            
+            
+            if ($A[$i] > $startPoint) {
+                $endPoint = $A[$i];
+                
+                
+                
+                $startPoint = $A[$i];
+            }
 
+            /*
             if ($i + 1 < $countEl && $A[$i] > $A[$i+1]) {
                 $k = 1;
                 $startPoint = $A[$i];
@@ -51,7 +80,7 @@ class GeoHelper
                         $i += $k - 1; //смещаем итератор на величину просчитанного участка
                     }
                 }
-            }
+            }*/
         }
 
         return "Max depth: $result";
